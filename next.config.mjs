4cab80de82d3f69ccb59e142b743ed82946e1d66
jalejandrov93 <jhonvasquez93@utp.edu.co>
2026-02-04 13:14:04 -0500

    feat: Add multi-stage Dockerfile for Next.js deployment, include a `robots.txt` to disallow crawling, and configure Next.js to remove console logs in production.

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   // Habilitar output standalone para Docker
   output: 'standalone',
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  }
 };
 
 export default nextConfig;
